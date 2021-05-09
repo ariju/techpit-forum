b1830d34a132179f1098aa30a331e6cc52034049
ariju <jun.0309ari@gmail.com>
2021-05-09 15:01:35 +0900

    react-router-domをインストール

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import Header from './components/Header'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import PageHome from './components/PageHome'
+import PageThread from './components/PageThread'
+import PageNotFound from './components/PageNotFound'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={'/'}>
+          <PageHome />
+        </Route>
+        <Route exact path={'/threads/:threadId'}>
+          <PageThread />
+        </Route>
+        <Route path={'*'}>
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
